d536688534e7bb0a5602e2f878aa3de9c4a0e9a5
codermastr <65798699+codermastr@users.noreply.github.com>
2020-08-08 10:24:14 +0800

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,23 @@
+
+
+function preload(){
+  var dog
+}
+
+function setup() {
+  createCanvas(800, 700);
+  createSprite(x,y,50,50);
+  dog("black")
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  
+
+}
+
+
+
